ed8039b0c1aebb648a7928617288c09d13c08738
Syping <schiedelrafael@keppe.org>
2020-04-27 23:45:06 +0200

    Update Conversations

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Conversations
-LOCAL_SRC_FILES := eu.siacs.conversations_364.apk
+LOCAL_SRC_FILES := eu.siacs.conversations_367.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
